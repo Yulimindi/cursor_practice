7b128f32d62905a37b5a0900aa122dc382752c6d
yulim <lim016@seesunit.com>
2026-07-02 11:01:07 +0900

    Initial commit: company intro page, apple game, Railway deploy config

---
+++ src/main/java/com/example/demo/PracticeCursorApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeCursorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeCursorApplication.class, args);
+	}
+
+}
